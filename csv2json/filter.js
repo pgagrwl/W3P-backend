2e3f5899cc79e57b36b738fa185ce7b4785f08d7
pgagrwl <pgagrwl@gmail.com>
2023-12-17 16:57:51 +0530

    initial commit

---
+++ csv2json/filter.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const keccak256 = require('keccak256');
+const filterArr = []
+
+fs.readFile('./ allMetadata.json', 'ascii', function (err, data) {
+
+    const arr = JSON.parse(data)
+    let traitsObj = {}
+    // Display the file content 
+    arr.forEach(element => {
+        for (let i = 0; i < element.attributes.length; i++) {
+            traitsObj[element.attributes[i].trait_type] = element.attributes[i].value
+        }
+
+        const payload = {
+            "image": element.image,
+            "attributeCount": element.count,
+            "URI": element.URI,
+            "traits": traitsObj,
+            "tokenId": element.tokenId,
+            "imageHash": keccak256(element.image).toString('hex')
+        }
+
+        filterArr.push(payload)
+        traitsObj = {}
+    });
+
+
+    fs.writeFile(`data.json`, JSON.stringify(filterArr), (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("File written successfully\n");
+        }
+    });
+
+});
